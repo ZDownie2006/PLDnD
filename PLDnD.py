7589fc26c4c00eb2ec2cf2f4ece20e3cdd70a181
Yasmin So <yasmin@yasmin.so>
2026-09-24 15:40:42 +1000

    fix: escape seqence warning

--- PLDnD.py
+++ PLDnD.py
@@ -3,10 +3,7 @@
 import curses
 import characters
 
-
 def main(window: curses.window):
-
-    stdscr = curses.initscr()
 
     window.keypad(True)
     curses.noecho()
@@ -29,7 +26,7 @@ def main(window: curses.window):
     window.addstr(1, pos_mid - 6, " _   |~  _ ")
     window.addstr(2, pos_mid - 6, "[_]--'--[_]")
     window.addstr(3, pos_mid - 6, "|'|  `  |'|")
-    window.addstr(4, pos_mid - 6, "| | /^\ | |")
+    window.addstr(4, pos_mid - 6, "| | /^\\ | |")
     window.addstr(5, pos_mid - 6, "|_|_|_|_|_|")
     window.addstr(7, pos_mid - 10, "Welcome to PLDnD!!!")
 
@@ -47,19 +44,21 @@ def main(window: curses.window):
     char_table.addstr(3, 22, "{}".format(characters.fighter.role_type))
     char_table.addstr(3, 32, "{}".format(characters.fighter.hp))
     char_table.addstr(3, 42, "{}".format(characters.fighter.ac))
-    char_table.addstr(3, 52, "{}".format(characters.fighter.attack))
+    char_table.addstr(3, 52, "+" + "{}".format(characters.fighter.at_mod))
     char_table.addstr(3, 62, "{}".format(characters.fighter.dmg))
     char_table.addstr(4, 2, "{}".format(characters.goblin.name))
     char_table.addstr(4, 12, "{}".format(characters.goblin.role))
     char_table.addstr(4, 22, "{}".format(characters.goblin.role_type))
     char_table.addstr(4, 32, "{}".format(characters.goblin.hp))
     char_table.addstr(4, 42, "{}".format(characters.goblin.ac))
-    char_table.addstr(4, 52, "{}".format(characters.goblin.attack))
+    char_table.addstr(4, 52, "+" + "{}".format(characters.goblin.at_mod))
     char_table.addstr(4, 62, "{}".format(characters.goblin.dmg))
 
     while 1:  # Loop so the game doesn't exit instantly
         window.refresh()  # Refresh
-
+        c = window.get_wch()
+        if c == 'x':
+            break
 
 if __name__ == '__main__':
     curses.wrapper(main)  # Initialise and return the window to main()
--- characters.py
+++ characters.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
+import random
+
 class Characters:
-    def __init__(self, name, role, role_type, hp, ac, attack, dmg):
+    def __init__(self, name, role, role_type, hp, ac, at_mod, dmg):
         self.name = name
         self.role = role
         self.role_type = role_type
         self.hp = hp
         self.ac = ac
-        self.attack = attack
+        self.at_mod = at_mod
         self.dmg = dmg
 
-fighter = Characters("Jeff", "Player", "Fighter", 10, 12, 18, 10)
-goblin = Characters("Boggard", "Enemy", "Goblin", 8, 15, 17, 7)
+fighter = Characters("Jeff", "Player", "Fighter", 10, 12, 5, 10)
+goblin = Characters("Boggard", "Enemy", "Goblin", 8, 15, 3, 7)
